faa4a87f8ba0edc24520328cd726c1f93a5a6e2a
SophiaNM <njerimurage92@outlook.com>
2018-07-21 19:52:12 +0300

    view function for search

--- instagram/views.py
+++ instagram/views.py
@@ -1,6 +1,6 @@
 from django.http  import HttpResponse, Http404
 from django.shortcuts import render, redirect
-# from .models import
+from .models import Image, Profile, Comments
 
 
 # Create your views here.
@@ -9,3 +9,16 @@ def index(request):
 
     title = 'Instaclone'
     return render(request, 'index.html', {'title':title})
+
+def search_results(request):
+
+    if 'username' in request.GET and request.GET["username"]:
+        search_term = request.GET.get("username")
+        searched_users = Profile.search_profile(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"users": searched_users})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
