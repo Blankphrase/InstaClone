6332eb491c4eb713dc7ca2e5e4405d32bb9a7098
SophiaNM <njerimurage92@outlook.com>
2018-07-28 18:11:09 +0300

    Changing index view

--- instagram/urls.py
+++ instagram/urls.py
@@ -4,8 +4,8 @@ from . import views
 from django.conf.urls.static import static
 
 urlpatterns=[
-    url('^$',views.index,name = 'index'),
-    url('^home/',views.home,name = 'home'),
+    url('^home/',views.index,name = 'index'),
+    url('^$',views.home,name = 'home'),
     url(r'^signup/$', views.signup, name='signup'),
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',views.activate, name='activate'),
     url(r'^search/', views.search_results, name='search_results'),
@@ -14,6 +14,7 @@ urlpatterns=[
     url(r'^upload/$', views.upload_image, name='upload_image'),
     # url(r'^friendship/', include('friendship.urls')),
     url(r'^follow/(?P<user_id>\d+)', views.follow, name='follow'),
+    url(r'^unfollow/(?P<user_id>\d+)', views.unfollow, name='unfollow'),
     url(r'^comment/(?P<image_id>\d+)', views.comment, name='comment'),
     url(r'^like/(?P<image_id>\d+)', views.like, name='like'),
     url(r'^is_liked/', views.is_liked, name='is_liked'),
