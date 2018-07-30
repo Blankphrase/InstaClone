de00c6ca1f5cab191ba6982891142baf43e102f3
SophiaNM <njerimurage92@outlook.com>
2018-07-30 16:00:38 +0300

    modifying tests for the class methods

--- instagram/tests.py
+++ instagram/tests.py
@@ -10,7 +10,7 @@ class ProfileTestClass(TestCase):
         test case for profiles
         '''
         self.user = User.objects.create_user('testuser','password')
-        self.profile = Profile(bio='I am a testcase',photo='', user=self.user)
+        self.profile = Profile(bio='I am a testcase',photo='', user='')
         self.profile.save_profile()
 
     def test_instance(self):
@@ -27,32 +27,34 @@ class ProfileTestClass(TestCase):
         profile = Profile.objects.all()
         self.assertTrue(len(profiel) == 0)
 
-    def test_update(self):
-        location = Location.get_location_id(self.loc.id)
-        location.update_location('Donholm')
-        location = Location.get_location_id(self.loc.id)
-        self.assertTrue(location.name == 'Donholm')
+    def test_search_by_profile(self):
+        profiles = Profile.search_profile('')
+        self.assertTrue(len(profiles)>0
 
 # Test for Image class
 class ImageTestClass(TestCase):
     # Set up Method
     def setUp(self):
-        self.cat = Category(name="fashion")
-        self.cat.save_category()
+        self.profile = Profile(bio='I am a testcase',photo='',user='')
+        self.proile.save_profile()
 
-        self.loc = Location(name="Africa")
-        self.loc.save_location()
 
-        self.image = Image(name='image test', description='my test',image_location=self.loc, image_category=self.cat)
+        self.image = Image(name='image test', picture='my test',caption='a caption test', profile='', profile_det=self.profile)
         self.image.save_image()
+
+        self.likes = Likes(image = self.image, user = '')
+        self.likes.save_like()
+
+        self.comments = Comments(comment='',image=self.image, user = '')
+        self.comment.save_comment()
+
 
     def test_instance(self):
         self.assertTrue(isinstance(self.image, Image))
 
     def tearDown(self):
         self.image.delete_image()
-        self.cat.delete_category()
-        self.loc.delete_location()
+        self.profile.delete_profile()
 
 
     def test_save_method(self):
@@ -69,18 +71,76 @@ class ImageTestClass(TestCase):
         images= Image.get_image_by_id(self.image.id)
         self.assertTrue(len(images) == 1)
 
-    def test_search_by_category(self):
-        images = Image.search_by_category('fashion')
+    def test_get_profile_pic(self):
+        images = Image.get_profile_pic(profile.id)
         self.assertTrue(len(images)>0)
 
-    def test_filter_by_location(self):
-        images = Image.fil0ter_by_location('1')
-        print(images)
-        self.assertTrue(len(images)>0)
+    def test_count_likes(self):
+        images = Image.count_likes()
+        self.assertTrue(images.likes.count()>0)
 
-    def test_update_image(self):
+    def test_count_comments(self):
+        images = Image.count_comments()
+        self.assertTrue(len(images.comments.count())>0)
+
+
+
+class LikesTest(TestCase):
+    #Set up Method
+    def setUp(self):
+        self.profile = Profile(bio='I am a testcase',photo='',user='')
+        self.profile.save_profile()
+
+        self.image = Image(name='image test', picture='my test',caption='a caption test', profile='', profile_det=self.profile)
         self.image.save_image()
-        image = Image.update_image( self.image.id, 'test update', 'my test',self.loc, self.cat)
-        upimage = Image.objects.filter(id = self.image.id)
-        print(upimage)
-        self.assertTrue(Image.name == 'test update')
+
+        self.likes = Likes(image = self.image, user = '')
+        self.likes.save_like()
+
+        self.comments = Comments(comment='',image=self.image, user = '')
+        self.comment.save_comment()
+
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.likes,Like))
+
+    def test_save_method(self):
+        self.profile.save_like()
+        like = Like.objects.all()
+        self.assertTrue(len(like) > 0)
+
+    def test_unlike_method(self):
+        self.like.save_like()
+        self.like.unlike_like()
+        like = Like.objects.all()
+        self.assertTrue(len(like) == 0)
+
+class CommentsTest(TestCase):
+    #Set up Method
+    def setUp(self):
+        self.profile = Profile(bio='I am a testcase',photo='',user='')
+        self.proile.save_profile()
+
+        self.image = Image(name='image test', picture='my test',caption='a caption test', profile='', profile_det=self.profile)
+        self.image.save_image()
+
+        self.likes = Likes(image = self.image, user = '')
+        self.likes.save_like()
+
+        self.comments = Comments(comment='',image=self.image, user = '')
+        self.comment.save_comment()
+
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.comments,Comments))
+
+    def test_save_method(self):
+        self.comments.save_comment()
+        comment = Comments.objects.all()
+        self.assertTrue(len(comments) > 0)
+
+    def test_delete_method(self):
+        self.comments.save_comment
+        self.comments.delete_comment()
+        comments = Comments.objects.all()
+        self.assertTrue(len(comments) == 0)
